d8735bd91dd97a5282d036a3d5074705fdaeef29
JavierLopez-UAO <javier.lopez@uao.edu.co>
2023-03-28 22:02:49 -0500

    a
    a

--- script_servidor3.sh
+++
@@ -1,8 +0,0 @@
-# Script de Aprovisionamiento (servidor3)
-#!/usr/bin/env bash
-
-yum install vim -y
-dnf update rpm -y
-dnf update -y
-yum install bind-utils bind-libs bind-* -y
-service named start
